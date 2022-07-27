273743239a1fde90f59f3c72effc7840dd8c1d43
“Samay <chaudharysamay98@gmail.com>
2022-07-27 18:33:21 +0545

    :sparkles: closure

---
+++ builtin_function2.py
@@ -0,0 +1,68 @@
+# >>>>>advance min() and max() function<<<<<
+
+def any_fun(name):
+    return len(name)   #this function euta euta name lincha ani length dincha
+
+names = ["apple","joy","om","ram kumar"]
+names2 = [1,2,3,4,5,6,7,8,9]
+print (max(names,key=any_fun))       #gives the name which has most letters as len is given in function.
+print (max(names2))
+
+#using lambda
+print (max(names,key=lambda name:len(name)))
+
+
+#1) find the name of highest marks
+
+student1 = [                                 #[{},{},{}]
+    {"name":"Ram","score":60,"age":19},
+{"name":"kiran","score":70,"age":20},
+{"name":"om","score":80,"age":21}
+]
+
+student2 = {"Ram":{"score":60,"age":19},      #dictionaries of dictionaries
+"kiran":{"score":70,"age":20},
+"om":{"score":80,"age":21}}
+
+"""def topper(name):
+    max_value = 0
+    all_values = []
+    for item in name:
+        for key,value in item.items():
+            all_values.append(value[0])
+            print(all_values)
+print(topper(student1))"""
+
+
+
+names = ["apple","joy","om","ram kumar"]
+names2 = [1,2,3,4,5,6,7,8,9]
+
+#sort method
+# name1.sort()
+# print name1
+
+#but 
+
+
+#1) find the sorted accordimg to highest marks:
+
+student1 = [                                 #[{},{},{}]
+    {"name":"Ram","score":60,"age":19},
+{"name":"kiran","score":70,"age":20},
+{"name":"om","score":80,"age":21}
+]
+
+sorted_list = sorted(student1, key=lambda item:item.get("name"))
+print (sorted_list)
+
+
+sorted_list = sorted(student1, key=lambda item:item.get("score"),reverse = True)
+print (sorted_list)
+
+sorted_list = sorted(student1, key=lambda item:item.get("score"))
+print (sorted_list)
+
+
+# doc function done..
+
---
+++ decorators.py
@@ -0,0 +1,84 @@
+"""
+------------>  Closure  <------------
+------------------------------------------
+--> First class function / closure
+--> it make unique python and many lecturer skip this topic
+
+"""
+
+# 0) Intro  -------------------------------->
+import re
+
+
+def square(a):
+	return a**2
+
+s = square  # not calling function only assigning
+
+print(s(7)) # but we can call and treat s as a square function
+print(s.__name__)
+print(square.__name__)
+
+# 1) pass function as a argumnet    ------------->
+# --> for eg: map function 
+any_list = [1,2,3]
+print(list(map(lambda a:a**2, any_list)))
+
+
+any_list2 = [4,5,6]
+
+def my_own_map(func,list):  # create using list comprehension
+	new_list = []
+	for item in list:
+		new_list.append(func(item))
+	return new_list
+
+
+def cube(num):
+    return num**3
+
+
+print(list(map(cube,[1,2,3])))
+
+def mero_aafnai_map(fun,list):
+    new_list=[]
+    for ele in list:
+        new_list.append(fun(ele))
+    return new_list
+
+def sq(a):
+    return a**2
+
+
+print(mero_aafnai_map(cube,[4,5,6] ))
+
+
+# 2) function return function  ---------------->
+
+def out_func():
+	def in_func():
+		print("Hello done")
+	return in_func
+
+any_var = out_func()
+any_var() # calling second function
+
+
+def out_func(value1):
+	def in_func(value2):
+		print(f"Hello done any-->{value1} and ok --> {value2} ")
+	return in_func
+
+any_var = out_func("ok1")
+any_var("any1") # calling second function
+
+# practice 1: 
+def cube_off(num):
+	def calc_power(n):
+		return num**n
+	return calc_power
+
+to_power = cube_off(4)
+print(to_power(3))
+
+
